b7b33c5f9aec16a02898d094d0baaad502f87d79
safetyblades <142550154+safetyblades@users.noreply.github.com>
2023-09-06 16:54:52 +0100

    Added files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace RockPaperScissors
+{
+    public class Program
+    {
+        public static void MyMethod()
+        {
+            // Repeat the game
+            Console.WriteLine("Do you want to play again? : Y/N");
+            string playAgain = Console.ReadLine();
+
+            if (playAgain == "Y" || playAgain == "y" || playAgain == "Yes" || playAgain == "yes")
+            {
+                TheGame();
+            }
+            else
+            {
+                Console.WriteLine("Thank you for playing");
+            }
+        }
+        public static void TheGame()
+        {
+            string[] choice = {"R", "P", "S"};
+            Random random = new Random();
+
+            // Get bot answer
+            int botAnswer;
+            botAnswer = random.Next(0,2);
+
+            // Get user answer
+            Console.WriteLine("Choose between one of these : R = Rock, P = Paper, S = Scissors");
+            string playerAnswer = Console.ReadLine();
+
+            // Prints out what the bot has chosen
+            if (botAnswer == 0)
+            {
+                Console.WriteLine("The computer chose: Rock");
+            }
+            else if (botAnswer == 1)
+            {
+                Console.WriteLine("The computer chose: Paper");
+            }
+            else
+            {
+                Console.WriteLine("The computer chose: Scissors");
+            }
+
+            // Checks who wins
+            if (playerAnswer == "r" || playerAnswer == "R")
+            {
+                if (botAnswer == 0)
+                {
+                    Console.WriteLine("Draw");
+                    MyMethod();
+                }
+                else if (botAnswer == 1)
+                {
+                    Console.WriteLine("You lost");
+                    MyMethod();
+                }
+                else
+                {
+                    Console.WriteLine("You win");
+                    MyMethod();
+                }
+            }
+            else if (playerAnswer == "p" || playerAnswer == "P")
+            {
+                if (botAnswer == 0)
+                {
+                    Console.WriteLine("You win");
+                    MyMethod();
+                }
+                else if (botAnswer == 1)
+                {
+                    Console.WriteLine("Draw");
+                    MyMethod();
+                }
+                else
+                {
+                    Console.WriteLine("You lost");
+                    MyMethod();
+                }
+            }
+            else
+            {
+                if (botAnswer == 0)
+                {
+                    Console.WriteLine("You lost");
+                    MyMethod();
+                }
+                else if (botAnswer == 1)
+                {
+                    Console.WriteLine("You win");
+                    MyMethod();
+                }
+                else
+                {
+                    Console.WriteLine("Draw");
+                    MyMethod();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Welcome
+            Console.WriteLine("Welcome to my Rock/Paper/Scissors");
+            TheGame();
+        }
+    }   
+}
